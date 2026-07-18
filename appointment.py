25532898c13a8c4620413be28d4b3ef5581d87b4
Akash89-eng <a6154496251@gmail.com>
2026-07-18 22:15:23 +0530

    Added login feature module

--- appointment.py
+++ appointment.py
@@ -1,2 +1,5 @@
 print("Hospital Appointment Booking System")
-print("Patient Registration Module loaded.")#added line   
+print("Patient Registration Module loaded.")#added line 
+#Added login functionallity 
+def login():
+    print("user login system")
